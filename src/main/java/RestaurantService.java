09295bca5bb50312858203bc6356566f02dc9c55
SrinathKadam048 <srinathkadam04@gmail.com>
2023-04-11 01:42:11 +0530

    Part 2: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -5,9 +5,16 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
-        return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        for(Restaurant restaurant : restaurants)
+        {
+            String storedRestaurantName = restaurant.getName();
+            if(restaurantName.equals(storedRestaurantName))
+            {
+                return restaurant;
+            }
+        }
+        throw new restaurantNotFoundException(restaurantName + " Not Found");
     }
 
 
